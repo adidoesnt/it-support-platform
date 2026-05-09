2166600b174bd161651b0f87951277aafabcffb8
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-05-09 15:36:42 +0100

    add update ticket method to ticket service

--- isp-backend/src/main/java/com/adidoesnt/itsupportplatform/ticket/TicketService.java
+++ isp-backend/src/main/java/com/adidoesnt/itsupportplatform/ticket/TicketService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -36,6 +37,31 @@ public class TicketService {
     public Optional<Ticket> getTicketById(Long id) {
         Optional<Ticket> ticket = ticketRepository.findById(id);
         return ticket;
+    }
+
+    @Transactional
+    public Ticket updateTicketById(
+            Long id,
+            Optional<String> title,
+            Optional<String> description,
+            Optional<TicketStatus> status) throws IllegalArgumentException, EntityNotFoundException {
+        if (title.isEmpty() && description.isEmpty() && status.isEmpty()) {
+            throw new IllegalArgumentException("At least one of title, description, or status must be provided");
+        }
+
+        Optional<Ticket> ticket = ticketRepository.findById(id);
+        if (ticket.isEmpty()) {
+            throw new EntityNotFoundException("Ticket not found");
+        }
+
+        Ticket ticketToSave = ticket.get();
+
+        title.ifPresent(ticketToSave::setTitle);
+        description.ifPresent(ticketToSave::setDescription);
+        status.ifPresent(ticketToSave::setStatus);
+
+        Ticket updatedTicket = ticketRepository.save(ticketToSave);
+        return updatedTicket;
     }
 
     @Transactional
