70ae866fbb3826076cb1c45035246dbb0038f2dc
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-05-09 13:04:35 +0100

    initial commit

---
+++ isp-backend/src/main/java/com/adidoesnt/itsupportplatform/ItsupportplatformApplication.java
@@ -0,0 +1,13 @@
+package com.adidoesnt.itsupportplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsupportplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsupportplatformApplication.class, args);
+	}
+
+}
---
+++ isp-backend/src/test/java/com/adidoesnt/itsupportplatform/ItsupportplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidoesnt.itsupportplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsupportplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
